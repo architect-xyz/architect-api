65de554bb64d2b20eafaa1bd1e7c878fc729647e
andrew-lee-architect <126016409+andrew-lee-architect@users.noreply.github.com>
2024-01-18 01:32:52 -0600

    alee/auth0 (#127)
    * GraphQL server now supports Auth0 JWT authentication; piped thru to the context as an AuthInfo
    * architect-userdb get-access-token test utility
    * Worked out the fundamentals for "customdb" in GraphQL
    * Built Market::is_favorite functionality over this new customdb pattern
    * Fixed excessive context cloning cost
    
    Try out the new /me endpoint to see auth in action

--- src/config.rs
+++ src/config.rs
@@ -27,10 +27,14 @@ pub struct Config {
     /// UserDB registration servers
     #[serde(default = "Config::default_registration_servers")]
     pub registration_servers: Vec<String>,
+    /// Custom chain of trust for UserDB, for debugging
+    #[serde(default)]
+    pub registration_trust_chain: Option<String>,
     #[serde(default = "Config::default_hosted_base")]
     pub hosted_base: Path,
     #[serde(default = "Config::default_local_base")]
     pub local_base: Path,
+    // TODO: move this to core command-line argument?
     /// Where to mount this core's RPCs and channel; also identifies this core
     pub core_base: Path,
     /// Use local symbology instead of centralized symbology
@@ -42,6 +46,10 @@ pub struct Config {
     pub use_legacy_marketdata_paths: bool,
     #[serde(default)]
     pub marketdata_location_override: HashMap<String, Location>,
+    // TODO: these only make sense for core, API config should have some other way
+    // for that matter, who has a consistent view of topology?
+    //
+    // gateways should do some ComponentID translation, at a minimum
     /// Locally run components in the same process
     #[serde(default)]
     pub local: HashMap<ComponentId, (String, serde_json::Value)>,
