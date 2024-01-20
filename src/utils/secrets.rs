5f64685a704545e6216559cbd667ae0b86815b7e
andrew-lee-architect <126016409+andrew-lee-architect@users.noreply.github.com>
2024-01-20 15:06:35 -0600

    alee/more secrets (#138)
    Make MaybeSecrets more ergonomic

--- src/utils/secrets.rs
+++ src/utils/secrets.rs
@@ -1,46 +1,89 @@
 //! Types for working with the secret store
 
-use netidx::pack::Pack;
-use serde_with::{DeserializeFromStr, SerializeDisplay};
-use std::str::FromStr;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::{fmt::Display, str::FromStr};
+use zeroize::{Zeroize, Zeroizing};
 
 /// A type that is either a reference to a secret, serialized as
 /// a URI string like secrets://<key>, or a plain literal.
-#[derive(
-    Debug,
-    Clone,
-    PartialEq,
-    Eq,
-    PartialOrd,
-    Ord,
-    Hash,
-    DeserializeFromStr,
-    SerializeDisplay,
-)]
-pub enum MaybeSecret<T: Pack + FromStr> {
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum MaybeSecret<T: Zeroize> {
     Secret(String),
-    // no zeroize here--if you're keeping a secret in plaintext config,
-    // you've already lost; this is for debugging and test
-    Plain(T),
+    Plain(Zeroizing<T>),
 }
 
-impl<T: std::fmt::Display + Pack + FromStr> std::fmt::Display for MaybeSecret<T> {
+impl<T: Zeroize> MaybeSecret<T> {
+    pub fn secret<S: AsRef<str>>(key: S) -> Self {
+        MaybeSecret::Secret(key.as_ref().to_string())
+    }
+
+    pub fn plain(t: T) -> Self {
+        MaybeSecret::Plain(Zeroizing::new(t))
+    }
+}
+
+impl<T: Display + Serialize + Zeroize> Display for MaybeSecret<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
+        match &*self {
             MaybeSecret::Secret(s) => write!(f, "secrets://{}", s),
-            MaybeSecret::Plain(s) => write!(f, "{}", s),
+            MaybeSecret::Plain(s) => {
+                write!(f, "{}", serde_json::to_string(&**s).map_err(|_| std::fmt::Error)?)
+            }
         }
     }
 }
 
-impl<T: Pack + FromStr> FromStr for MaybeSecret<T> {
-    type Err = <T as FromStr>::Err;
+impl<T: FromStr + DeserializeOwned + Zeroize> FromStr for MaybeSecret<T> {
+    type Err = serde_json::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s.starts_with("secrets://") {
             Ok(MaybeSecret::Secret(s[10..].to_string()))
         } else {
-            Ok(MaybeSecret::Plain(s.parse()?))
+            Ok(MaybeSecret::Plain(Zeroizing::new(serde_json::from_str(s)?)))
+        }
+    }
+}
+
+impl<T: Serialize + Zeroize> Serialize for MaybeSecret<T> {
+    fn serialize<S: serde::Serializer>(&self, ser: S) -> Result<S::Ok, S::Error> {
+        match self {
+            MaybeSecret::Secret(s) => ser.serialize_str(&format!("secrets://{}", s)),
+            MaybeSecret::Plain(t) => (&*t).serialize(ser),
+        }
+    }
+}
+
+impl<'de, T: DeserializeOwned + FromStr + Zeroize> Deserialize<'de> for MaybeSecret<T> {
+    fn deserialize<D>(de: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Serialize, Deserialize)]
+        #[serde(untagged)]
+        enum Format<T> {
+            SecretOrString(String),
+            Plain(T),
+        }
+        match Format::<T>::deserialize(de)? {
+            Format::SecretOrString(s) => {
+                if s.starts_with("secrets://") {
+                    Ok(MaybeSecret::Secret(s[10..].to_string()))
+                } else {
+                    // using FromStr here is hacky but it works for the
+                    // important cases of T = String, &str, etc... at
+                    // the cost of requiring FromStr from structs
+                    //
+                    // maybe there's some trick leveraging auto(de)ref
+                    // specialization [https://lukaskalbertodt.github.io/2019/12/05/generalized-autoref-based-specialization.html]
+                    // that could help here?
+                    Ok(MaybeSecret::Plain(Zeroizing::new(
+                        T::from_str(&s)
+                            .map_err(|_| serde::de::Error::custom("could not FromStr"))?,
+                    )))
+                }
+            }
+            Format::Plain(t) => Ok(MaybeSecret::Plain(Zeroizing::new(t))),
         }
     }
 }
@@ -48,19 +91,78 @@ impl<T: Pack + FromStr> FromStr for MaybeSecret<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::bail;
+    use zeroize::ZeroizeOnDrop;
 
     #[test]
     fn test_from_str() {
         let x: MaybeSecret<u64> = "secrets://foo".parse().unwrap();
-        assert_eq!(x, MaybeSecret::Secret("foo".to_string()));
+        assert_eq!(x, MaybeSecret::secret("foo"));
         let y: MaybeSecret<u64> = "42".parse().unwrap();
-        assert_eq!(y, MaybeSecret::Plain(42));
+        assert_eq!(y, MaybeSecret::plain(42u64));
     }
 
     #[test]
-    fn test_serialize() {
-        let x: MaybeSecret<u64> = MaybeSecret::Secret("asdf".to_string());
-        let x2 = serde_json::to_string(&x).unwrap();
-        assert_eq!(x2, "\"secrets://asdf\"");
+    fn test_serde() {
+        let x: MaybeSecret<u64> = MaybeSecret::secret("asdf");
+        let y = serde_json::to_string(&x).unwrap();
+        let z = serde_json::from_str(&y).unwrap();
+        assert_eq!(x, z);
+        let x: MaybeSecret<u64> = MaybeSecret::plain(42);
+        let y = serde_json::to_string(&x).unwrap();
+        let z = serde_json::from_str(&y).unwrap();
+        assert_eq!(x, z);
+        let x: MaybeSecret<String> = MaybeSecret::plain("hahaha".to_string());
+        let y = serde_json::to_string(&x).unwrap();
+        let z = serde_json::from_str(&y).unwrap();
+        assert_eq!(x, z);
+    }
+
+    #[test]
+    fn test_serde_yaml() {
+        let x: MaybeSecret<u64> = MaybeSecret::secret("asdf");
+        let y = serde_yaml::to_string(&x).unwrap();
+        let z = serde_yaml::from_str(&y).unwrap();
+        assert_eq!(x, z);
+        let x: MaybeSecret<u64> = MaybeSecret::plain(42);
+        let y = serde_yaml::to_string(&x).unwrap();
+        let z = serde_yaml::from_str(&y).unwrap();
+        assert_eq!(x, z);
+        let x: MaybeSecret<String> = MaybeSecret::plain("hahaha".to_string());
+        let y = serde_yaml::to_string(&x).unwrap();
+        let z = serde_yaml::from_str(&y).unwrap();
+        assert_eq!(x, z);
+    }
+
+    #[test]
+    fn test_serde_complex() {
+        #[derive(
+            Debug, PartialEq, Eq, Serialize, Deserialize, Zeroize, ZeroizeOnDrop,
+        )]
+        struct Foo {
+            bar: u64,
+            baz: String,
+        }
+        impl FromStr for Foo {
+            type Err = anyhow::Error;
+            fn from_str(_: &str) -> Result<Self, Self::Err> {
+                bail!("ambiguous format")
+            }
+        }
+        let x: MaybeSecret<Foo> =
+            MaybeSecret::plain(Foo { bar: 42, baz: "asdf".to_string() });
+        let y = serde_json::to_string(&x).unwrap();
+        let z = serde_json::from_str(&y).unwrap();
+        assert_eq!(x, z);
+        let yy = serde_yaml::to_string(&x).unwrap();
+        let zz = serde_yaml::from_str(&yy).unwrap();
+        assert_eq!(x, zz);
+        let x: MaybeSecret<Foo> = MaybeSecret::secret("my_secret_key");
+        let y = serde_json::to_string(&x).unwrap();
+        let z = serde_json::from_str(&y).unwrap();
+        assert_eq!(x, z);
+        let yy = serde_yaml::to_string(&x).unwrap();
+        let zz = serde_yaml::from_str(&yy).unwrap();
+        assert_eq!(x, zz);
     }
 }
