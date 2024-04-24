37088ba718cefb779b02b5790624b5b429a710e4
Jen Cecelia Reiss <jen@architect.xyz>
2024-04-23 23:46:50 -0500

    Jenr/bybit marketdata (#372)
    Adds Marketdata for Bybit, improves sym loading, adds `send_heartbeat`
    for protocol level heartbeats for OKX and Bybit

--- src/cpty/bybit.rs
+++ src/cpty/bybit.rs
@@ -3,10 +3,22 @@ use netidx_derive::Pack;
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 
+#[derive(
+    Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq, Hash, clap::ValueEnum, Pack,
+)]
+#[serde(rename_all = "lowercase")]
+pub enum ProductType {
+    Spot,
+    Linear,
+    Inverse,
+    Option,
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize, Pack)]
 pub struct BybitMarketInfo {
     pub tick_size: Decimal,
     pub step_size: Decimal,
+    pub product_type: ProductType,
 }
 
 impl NormalizedMarketInfo for BybitMarketInfo {
