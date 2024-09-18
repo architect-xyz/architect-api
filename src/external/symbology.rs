f5c34bcbd476b9a7058bc996701012c3e814a6dc
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2024-09-18 00:16:06 -0500

    _

--- src/external/symbology.rs
+++ src/external/symbology.rs
@@ -11,3 +11,13 @@ pub struct SymbologySnapshot {
     pub products: Vec<Product>,
     pub markets: Vec<Market>,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SymbologyUpdate {
+    pub epoch: DateTime<Utc>,
+    pub seqno: u64,
+    pub routes: Option<Vec<Route>>,
+    pub venues: Option<Vec<Venue>>,
+    pub products: Option<Vec<Product>>,
+    pub markets: Option<Vec<Market>>,
+}
